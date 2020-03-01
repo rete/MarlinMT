8bc95fe64f759b88ceded0ac3b4aa73d5a104404
Remi Ete <remi.ete@gmail.com>
2020-03-02 00:28:14 +0100

    AlgorithmTool inherits from Component

--- source/framework/include/marlin/AlgorithmTool.h
+++ source/framework/include/marlin/AlgorithmTool.h
@@ -1,13 +1,31 @@
 #pragma once
 
-#include <marlin/Property.h>
+// -- marlin headers
+#include <marlin/Component.h>
 
 namespace marlin {
-    
-  class AlgorithmTool : public Configurable {
+  
+  /**
+   *  @brief  AlgorithmTool class
+   *  Re-usable algorithm tool used inside algorithms. Algorithms tools
+   *  can be either create locally in algorithms or created globally in
+   *  the application.
+   */
+  class AlgorithmTool : public Component {
   public:
-    AlgorithmTool() = default ;
+    /// No default constructor
+    AlgorithmTool() = delete ;
     virtual ~AlgorithmTool() = default ;
+
+    /**
+     *  @brief  Constructor with algorithm tool name 
+     * 
+     *  @param  name the algorithm tool name
+     */
+    inline AlgorithmTool( const std::string &name ) : 
+      Component(name) {
+      /* nop */
+    }
   };
   
 }
